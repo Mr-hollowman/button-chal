f56e440587609025891cc38cb10c76e91d4c2265
mr-hollowman <selvamani825@gmail.com>
2023-05-11 21:00:59 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import MyButton from "./MyButton";
 import "./App.css";
-import MySidebar from "./Sidebar";
-import Right from "./Right";
-import Left from "./Left";
 
 function App() {
   return (
-    <div className="d-flex">
-      <div style={{ maxWidth: "200px" }}>
-        <MySidebar />
-      </div>
-      <div className="d-flex" style={{width:'100%', margin:"10px"}}>
-        <Left />
-        <Right />
-      </div>
+    <div>
+      <MyButton color={"secondary"} />
+      <MyButton />
+      <MyButton />
     </div>
   );
 }
